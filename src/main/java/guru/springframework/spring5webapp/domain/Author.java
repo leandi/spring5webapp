45201e209ef8ac0694cd91ed553ffbf656d568b8
leandi <sandi.kurtin@gmail.com>
2021-05-16 20:12:35 +0200

    22 narejen A naloga zaključena.

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -66,7 +66,7 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                
+
                 '}';
     }
 
